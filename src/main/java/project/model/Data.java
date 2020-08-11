8ccf453ac8405955b5d2e41d9e76eb6492f556f0
Arish Myckel Abalos <ama541@uclive.ac.nz>
2020-08-11 15:12:48 +1200

    Revert "Merge remote-tracking branch 'origin/master' into master"
    This reverts commit 79a4f2347c8a45d727566f3f1b76647ecd737649

--- src/main/java/project/model/Data.java
+++ src/main/java/project/model/Data.java
@@ -1,13 +1,4 @@
 package project.model;
 
-public abstract class Data {
-    private String dataType;
-
-    public String getDataType() {
-        return this.dataType;
-    }
-
-    public abstract String print();
-
-    public abstract void update();
+public class Data {
 }
