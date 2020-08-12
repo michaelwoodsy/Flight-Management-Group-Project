4b2ed2a939eea36a53841dd6c678c126f7f09456
Louis <lfg23@uclive.ac.nz>
2020-08-12 16:29:48 +1200

    Fleshing out record class methods, a few tests for these methods

--- src/main/java/project/model/Record.java
+++ src/main/java/project/model/Record.java
@@ -3,52 +3,162 @@ package project.model;
 import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Record {
-    //ArrayList for each need to be flight objects, currently string but will need to change
-    private ArrayList<String> Flight = new ArrayList<String>();
-    private ArrayList<String> Route = new ArrayList<String>();
-    private ArrayList<String> Airport = new ArrayList<String>();
-    private ArrayList<String> Airline = new ArrayList<String>();
+    private ArrayList<Flight> flightList = new ArrayList<Flight>();
+    private ArrayList<Route> routeList = new ArrayList<Route>();
+    private ArrayList<Airport> airportList = new ArrayList<Airport>();
+    private ArrayList<Airline> airlineList = new ArrayList<Airline>();
 
-    public ArrayList<String> filterAirports(String country){
-        //skeleton code
-        return Airport;
+    public Record(ArrayList<Flight> flightList, ArrayList<Route> routeList, ArrayList<Airport> airportList, ArrayList<Airline> airlineList) {
+        this.flightList = flightList;
+        this.routeList = routeList;
+        this.airportList = airportList;
+        this.airlineList = airlineList;
     }
-    public ArrayList<String> filterAirlines(Boolean active){
-        return Airline;
+
+    /**
+     * Filters airports by country entered. Returns a new list of airports meeting this criteria.
+     */
+    public ArrayList<Airport> filterAirports(String country) {
+        ArrayList<Airport> filteredAirports = new ArrayList<Airport>();
+        for (Airport airport: airportList) {
+            if (airport.getCountry().equals(country)) {
+                filteredAirports.add(airport);
+            }
+        }
+        return filteredAirports;
     }
-    public ArrayList<String> filterAirlinesCountry(String country){
-        return Airline;
+
+    /**
+     * Filters airlines by whether they're active or not. Returns a new list of airlines meeting this criteria.
+     */
+    public ArrayList<Airline> filterAirlines(Boolean active) {
+        ArrayList<Airline> filteredAirlines = new ArrayList<Airline>();
+        for (Airline airline: airlineList) {
+            if (airline.isActive() == active) {
+                filteredAirlines.add(airline);
+            }
+        }
+        return filteredAirlines;
     }
-    public ArrayList<String> filterRoutesDeparture(String country){
-        return Route;
+
+    /**
+     * Filters airlines by country entered. Returns a new list of airlines meeting this criteria.
+     */
+    public ArrayList<Airline> filterAirlinesCountry(String country) {
+        ArrayList<Airline> filteredAirlines = new ArrayList<Airline>();
+        for (Airline airline: airlineList) {
+            if (airline.getCountry().equals(country)) {
+                filteredAirlines.add(airline);
+            }
+        }
+        return filteredAirlines;
     }
-    public ArrayList<String> filterRoutesDestination(String county){
-        return Route;
+
+    /**
+     * Filters routes by source airport. Returns a new list of routes meeting this criteria.
+     * Needs changing from sourceAirport to country. Perhaps countries can be mapped to source airports?
+     */
+    public ArrayList<Route> filterRoutesDeparture(String sourceAirport){
+        ArrayList<Route> filteredRoutes = new ArrayList<Route>();
+        for (Route route: routeList) {
+            if (route.getSourceAirport().equals(sourceAirport)) {
+                filteredRoutes.add(route);
+            }
+        }
+        return filteredRoutes;
     }
-    public ArrayList<String> filterRoutesStops(boolean direct){
-        return Route;
+
+    /**
+     * Filters routes by destination airport. Returns a new list of routes meeting this criteria.
+     * Needs changing from destAirport to country. Perhaps countries can be mapped to destination airports?
+     */
+    public ArrayList<Route> filterRoutesDestination(String destAirport) {
+        ArrayList<Route> filteredRoutes = new ArrayList<Route>();
+        for (Route route: routeList) {
+            if (route.getDestAirport().equals(destAirport)) {
+                filteredRoutes.add(route);
+            }
+        }
+        return filteredRoutes;
     }
-    public ArrayList<String> filterRoutesEquipment(String equipment){
-        return Route;
+
+    /**
+     * Filters routes by whether they have stops or not. Returns a new list of routes meeting this criteria.
+     */
+    public ArrayList<Route> filterRoutesStops(boolean direct) {
+        ArrayList<Route> filteredRoutes = new ArrayList<Route>();
+        if (direct) {
+            for (Route route: routeList) {
+                if (route.getNumStops() == 0) {
+                    filteredRoutes.add(route);
+                }
+            }
+        } else {
+            for (Route route: routeList) {
+                if (route.getNumStops() != 0) {
+                    filteredRoutes.add(route);
+                }
+            }
+        }
+        return filteredRoutes;
     }
-    public ArrayList<String> rankAirports(boolean reverse){
-        return Airport;
+
+    /**
+     * Filters routes by their flight equipment. Returns a new list of routes meeting this criteria.
+     */
+    public ArrayList<Route> filterRoutesEquipment(String equipment) {
+        ArrayList<Route> filteredRoutes = new ArrayList<Route>();
+        for (Route route: routeList) {
+            if (route.getEquipment().equals(equipment)) {
+                filteredRoutes.add(route);
+            }
+        }
+        return filteredRoutes;
     }
-    public ArrayList<String> searchAirports(String search){
-        return Airport;
+
+    /**
+     * Ranks airports based on the number of routes they have.
+     * Can be either ascending or descending order depending on the reverse parameter.
+     * Returns new, sorted list of airports (doesn't override original airportList).
+     */
+    public ArrayList<Airport> rankAirports(boolean reverse) {
+        ArrayList<Airport> rankedAirports = airportList;
+        if (reverse) {
+            rankedAirports.sort(Comparator.comparing(Airport::getNumRoutes).reversed());
+        } else {
+            rankedAirports.sort(Comparator.comparing(Airport::getNumRoutes));
+        }
+        return rankedAirports;
     }
-    public ArrayList<String> searchFlights(String search){
-        return Flight;
+
+    /**
+     * Placeholders.
+     * What are we basing the search criteria on? Airport? Country? Same applies to function below.
+     */
+    public ArrayList<Airport> searchAirports(String search) {
+
+        return new ArrayList<Airport>();
     }
+    public ArrayList<Flight> searchFlights(String search) {
+
+        return new ArrayList<Flight>();
+    }
+
+    /**
+     * This will be tricky. return statement and parameters aren't concrete.
+     */
     public void addRecord(InputStream inFile, boolean newRecord){
 
     }
-    public void addData(int dataType){
+
+    public void addData(int dataType) {
 
     }
-    public void deleteData(int dataType){
+
+    public void deleteData(int dataType, int index) {
 
     }
 
---
+++ src/test/java/project/RecordTest.java
@@ -0,0 +1,127 @@
+package project;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import project.model.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class RecordTest {
+    private ArrayList<Flight> defaultFlightList = new ArrayList<Flight>();
+    private ArrayList<Route> defaultRouteList = new ArrayList<Route>();
+    private ArrayList<Airport> defaultAirportList = new ArrayList<Airport>();
+    private ArrayList<Airline> defaultAirlineList = new ArrayList<Airline>();
+
+    @Test
+    public void filterAirportsTest() {
+
+        Airport testAirport1 = new Airport(100, 500, "Test1", "New Zealand", "Test1", "Test1", 50, 0.0, "Test1", "Test1", "Test1", "Openflights", 50);
+        Airport testAirport2 = new Airport(100, 500, "Test2", "New Zealand", "Test2", "Test2", 50, 0.0, "Test2", "Test2", "Test2", "Openflights", 50);
+        Airport testAirport3 = new Airport(100, 500, "Test3", "Australia", "Test3", "Test3", 50, 0.0, "Test3", "Test3", "Test3", "Openflights", 50);
+        Airport testAirport4 = new Airport(100, 500, "Test4", "Australia", "Test4", "Test4", 50, 0.0, "Test4", "Test4", "Test4", "Openflights", 50);
+        Airport testAirport5 = new Airport(100, 500, "Test5", "New Zealand", "Test5", "Test5", 50, 0.0, "Test5", "Test5", "Test5", "Openflights", 50);
+
+
+        ArrayList<Airport> testAirportList = new ArrayList<Airport>();
+        testAirportList.add(testAirport1);
+        testAirportList.add(testAirport2);
+        testAirportList.add(testAirport3);
+        testAirportList.add(testAirport4);
+        testAirportList.add(testAirport5);
+
+        Record testRecord = new Record(defaultFlightList, defaultRouteList, testAirportList, defaultAirlineList);
+
+        ArrayList<Airport> filteredTestAirportList = testRecord.filterAirports("New Zealand");
+
+        ArrayList<Airport> comparisonAirportList = new ArrayList<Airport>();
+
+        comparisonAirportList.add(testAirport1);
+        comparisonAirportList.add(testAirport2);
+        comparisonAirportList.add(testAirport5);
+
+        assertEquals(comparisonAirportList, filteredTestAirportList);
+
+        filteredTestAirportList = testRecord.filterAirports("Australia");
+
+        comparisonAirportList = new ArrayList<Airport>();
+        comparisonAirportList.add(testAirport3);
+        comparisonAirportList.add(testAirport4);
+
+        assertEquals(comparisonAirportList, filteredTestAirportList);
+    }
+
+    @Test
+    public void filterAirlinesTest() {
+        Airline testAirline1 = new Airline(100, "Test1", true, "Test1", "Test1", "Test1", "Test1", "Test1");
+        Airline testAirline2 = new Airline(100, "Test2", false, "Test2", "Test2", "Test2", "Test2", "Test2");
+        Airline testAirline3 = new Airline(100, "Test3", true, "Test3", "Test3", "Test3", "Test3", "Test3");
+        Airline testAirline4 = new Airline(100, "Test4", false, "Test4", "Test4", "Test4", "Test4", "Test4");
+        Airline testAirline5 = new Airline(100, "Test5", true, "Test5", "Test5", "Test5", "Test5", "Test5");
+
+        ArrayList<Airline> testAirlineList = new ArrayList<Airline>();
+
+        testAirlineList.add(testAirline1);
+        testAirlineList.add(testAirline2);
+        testAirlineList.add(testAirline3);
+        testAirlineList.add(testAirline4);
+        testAirlineList.add(testAirline5);
+
+        Record testRecord = new Record(defaultFlightList, defaultRouteList, defaultAirportList, testAirlineList);
+
+        ArrayList<Airline> filteredTestAirlineList = testRecord.filterAirlines(true);
+
+        ArrayList<Airline> comparisonAirlineList = new ArrayList<Airline>();
+        comparisonAirlineList.add(testAirline1);
+        comparisonAirlineList.add(testAirline3);
+        comparisonAirlineList.add(testAirline5);
+
+        assertEquals(comparisonAirlineList, filteredTestAirlineList);
+
+        filteredTestAirlineList = testRecord.filterAirlines(false);
+
+        comparisonAirlineList = new ArrayList<Airline>();
+        comparisonAirlineList.add(testAirline2);
+        comparisonAirlineList.add(testAirline4);
+
+        assertEquals(comparisonAirlineList, filteredTestAirlineList);
+
+    }
+
+    @Test
+    public void filterAirlinesCountryTest() {
+        Airline testAirline1 = new Airline(100, "Test1", true, "New Zealand", "Test1", "Test1", "Test1", "Test1");
+        Airline testAirline2 = new Airline(100, "Test2", false, "New Zealand", "Test2", "Test2", "Test2", "Test2");
+        Airline testAirline3 = new Airline(100, "Test3", true, "Australia", "Test3", "Test3", "Test3", "Test3");
+        Airline testAirline4 = new Airline(100, "Test4", false, "New Zealand", "Test4", "Test4", "Test4", "Test4");
+        Airline testAirline5 = new Airline(100, "Test5", true, "U.K.", "Test5", "Test5", "Test5", "Test5");
+
+        ArrayList<Airline> testAirlineList = new ArrayList<Airline>();
+
+        testAirlineList.add(testAirline1);
+        testAirlineList.add(testAirline2);
+        testAirlineList.add(testAirline3);
+        testAirlineList.add(testAirline4);
+        testAirlineList.add(testAirline5);
+
+        Record testRecord = new Record(defaultFlightList, defaultRouteList, defaultAirportList, testAirlineList);
+
+        ArrayList<Airline> filteredTestAirlineList = testRecord.filterAirlinesCountry("New Zealand");
+
+        ArrayList<Airline> comparisonAirlineList = new ArrayList<Airline>();
+        comparisonAirlineList.add(testAirline1);
+        comparisonAirlineList.add(testAirline2);
+        comparisonAirlineList.add(testAirline4);
+
+        assertEquals(comparisonAirlineList, filteredTestAirlineList);
+
+        filteredTestAirlineList = testRecord.filterAirlinesCountry("Australia");
+
+        comparisonAirlineList = new ArrayList<Airline>();
+        comparisonAirlineList.add(testAirline3);
+
+        assertEquals(comparisonAirlineList, filteredTestAirlineList);
+    }
+}
