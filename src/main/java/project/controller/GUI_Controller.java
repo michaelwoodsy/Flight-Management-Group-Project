06a6df9344570b449e7933a6be45ea7d1709c7d6
Dan Chapman <djc244@uclive.ac.nz>
2020-08-18 15:01:05 +1200

    Implemented adding aiports from a loaded file

--- src/main/java/project/controller/GUI_Controller.java
+++ src/main/java/project/controller/GUI_Controller.java
@@ -3,6 +3,9 @@ package project.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import project.model.*;
+
+import java.util.ArrayList;
 
 /**
  * Class for working in while experimenting with GUI features
@@ -11,11 +14,23 @@ public class GUI_Controller {
 
     @FXML
     private ListView airportList;
+    private ArrayList<String> existingAirportCodes = new ArrayList<String>();
 
     /**
-     * Configure the airportList
+     * Adds new files to the airportList
      */
-    public void testMethod() {
+    public void addAirports(ArrayList<Airport> airports) {
+        //Add each new airport to the file
+        //Check the existing airports and add their unique codes to a file
+        ArrayList<Airport> addedAirports = (ArrayList<Airport>) airportList.getItems();
+
+        for (Airport airport: airports) {
+            if (!existingAirportCodes.contains(airport.getIata())) {
+                addedAirports.add(airport);
+                existingAirportCodes.add(airport.getIata());
+            }
+        }
+
         //Not multiselect by default
         airportList.getItems().addAll("test", "Test2", "test23");
         //Operation to enable multiselect
