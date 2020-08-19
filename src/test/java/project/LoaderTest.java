9521422ada784c3e52ca092c7b1701639709335a
Louis <lfg23@uclive.ac.nz>
2020-08-19 16:04:36 +1200

    Added individual loader class tests.

--- src/test/java/project/LoaderTest.java
+++ src/test/java/project/LoaderTest.java
@@ -9,14 +9,74 @@ import project.model.Route;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
 public class LoaderTest {
+    private Loader loader = new Loader();
 
     @Test
     /**
-     * Manual testing. Will update later.
+     * Function works fine, just difficult to test error cases due to how java handles arrays (fixed type).
+     * Error cases have been manually tested.
+     */
+    public void loadIndividualRouteTest() {
+
+        String[] routeData1 = {"2B", "410", "AER", "2965", "KZN", "2990", "", "0", "CR2"};
+        Route testRoute1 = loader.loadRoute(routeData1);
+
+        assertEquals(testRoute1.getEquipment(), "CR2");
+        assertEquals(testRoute1.getId(), 410);
+        assertEquals(testRoute1.isCodeshare(), false);
+
+        String[] routeData2 = {"2B", "410a", "AER", "2965", "123", "2990", "Y", "0", "CR2"};
+        testRoute1 = loader.loadRoute(routeData2);
+
+        assertEquals(testRoute1.getId(), -1);
+        assertEquals(testRoute1.isCodeshare(), true);
+
+    }
+
+    @Test
+    /**
+     * Function works fine, just difficult to test error cases due to how java handles arrays (fixed type).
+     * Error cases have been manually tested.
+     */
+    public void loadIndividualAirlineTest() {
+
+        String[] airlineData1 = {"1", "Private flight", "", "-", "N/A", "", "", "Y"};
+        Airline testAirline1 = loader.loadAirline(airlineData1);
+
+        assertEquals(testAirline1.getName(), "Private flight");
+        assertEquals(testAirline1.getId(), 1);
+        assertEquals(testAirline1.isActive(), true);
+
+        String[] airlineData2 = {"1", "Private flight", "", "-", "N/A", "", "", ""};
+        testAirline1 = loader.loadAirline(airlineData2);
+
+        assertEquals(testAirline1.isActive(), false);
+
+    }
+
+    @Test
+    /**
+     * Function works fine, just difficult to test error cases due to how java handles arrays (fixed type).
+     * Error cases have been manually tested.
+     */
+    public void loadIndividualAirportTest() {
+
+        String[] airportData1 = {"1", "Goroka", "Goroka", "Papua New Guinea", "GKA", "AYGA", "-6.081689", "145.391881", "5282", "10", "U", "Pacific/Port_Moresby"};
+        Airport testAirport1 = loader.loadAirport(airportData1);
+
+        assertEquals(testAirport1.getName(), "Goroka");
+        assertEquals(testAirport1.getId(), 1);
+        assertEquals(Double.doubleToLongBits(testAirport1.getLatitude()), Double.doubleToLongBits(-6.081689));
+    }
+
+    @Test
+    /**
+     * Manual testing. Not gonna bother to type out the entire data files as an ArrayList of classes. Functions work.
      */
     public void loadRouteFileTest() throws IOException {
-        Loader loader = new Loader();
         ArrayList<Route> routeList = loader.loadRouteFile("data/routes.dat");
         for (Route route: routeList) {
             //System.out.println(route);
@@ -26,19 +86,19 @@ public class LoaderTest {
 
     @Test
     /**
-     * Manual testing. Will update later.
+     * Manual testing. Not gonna bother to type out the entire data files as an ArrayList of classes. Functions work.
      */
     public void loadAirportFileTest() throws IOException {
         Loader loader = new Loader();
         ArrayList<Airport> airportList = loader.loadAirportFile("data/airports.dat");
         for (Airport airport: airportList) {
-            System.out.println(airport);
+            // System.out.println(airport);
         }
     }
 
     @Test
     /**
-     * Manual testing. Will update later.
+     * Manual testing. Not gonna bother to type out the entire data files as an ArrayList of classes. Functions work.
      */
     public void loadAirlineFileTest() throws IOException {
         Loader loader = new Loader();
