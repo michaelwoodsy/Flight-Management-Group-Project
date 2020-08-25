32811258739c96a997734c1155225a212fa3bc33
Dan Chapman <djc244@uclive.ac.nz>
2020-08-25 13:24:30 +1200

    Added methods to create individual tables in the database

--- src/main/java/project/controller/Database.java
+++ src/main/java/project/controller/Database.java
@@ -4,9 +4,7 @@ import java.sql.*;
 
 public class Database {
 
-    public static void createNewDatabase() {
-
-        String url = "jdbc:sqlite:./data/database.db";
+    public static void createNewDatabase(String url) {
 
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
@@ -19,12 +17,75 @@ public class Database {
         }
     }
 
-    public static void createAirportTable(String tableName) {
-        String url = "jdbc:sqlite:./data/database.db";
-        String sql = "CREATE TABLE IF NOT EXISTS "+tableName+" (\n"
-                ;
+    public static void createAirportTable(String url) {
+        String sql = "CREATE TABLE IF NOT EXISTS airports (\n"
+                + " id integer PRIMARY KEY,\n"
+                + " risk integer NOT NULL,\n"
+                + " altitude integer,\n"
+                + " numRoutesSource integer,\n"
+                + " numRoutesDest integer,\n"
+                + " totalRoutes integer,\n"
+                + " airportName text,\n"
+                + " city text,\n"
+                + " country text,\n"
+                + " iata text,\n"
+                + " icao text,\n"
+                + " destination text,\n"
+                + " timezoneString text,\n"
+                + " airportType text,\n"
+                + " airportSource text,\n"
+                + " latitude real,\n"
+                + " longitude real,\n"
+                + " timezone real\n"
+                + ");";
 
-        try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement()) {
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createAirlineTable(String url) {
+        String sql = "CREATE TABLE IF NOT EXISTS airlines (\n"
+                + " id integer PRIMARY KEY,\n"
+                + " airlineName text,\n"
+                + " country text,\n"
+                + " alias text,\n"
+                + " callsign text,\n"
+                + " icao text,\n"
+                + " iata text,\n"
+                + " active integer\n" //Boolean value, can only be either 1 or 0
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createRouteTable(String url) {
+        String sql = "CREATE TABLE IF NOT EXISTS routes (\n"
+                + " id integer PRIMARY KEY,\n"
+                + " sourceID integer,\n"
+                + " destID integer,\n"
+                + " numStops integer,\n"
+                + " airline text,\n"
+                + " sourceAirport text,\n"
+                + " destAirport text,\n"
+                + " equipment text,\n"
+                + " codeshare integer\n," //Boolean value, can only be either 1 or 0
+                + " FOREIGN KEY (sourceID)"
+                + "     REFERENCES airports (id)"
+                + " FOREIGN KEY (destID)"
+                + "     REFERENCES airports (id)"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
             stmt.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -32,9 +93,11 @@ public class Database {
     }
 
 
-
     public static void main(String[] args) {
-        createNewDatabase();
+        String url = "jdbc:sqlite:./data/database.db";
+        createNewDatabase(url);
+        createAirportTable(url);
+        createAirlineTable(url);
+        createRouteTable(url);
     }
-
 }
