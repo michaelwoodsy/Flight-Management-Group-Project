eb9f93bc878c10704d48b0ddcaf02e757c16acaa
Dan Chapman <djc244@uclive.ac.nz>
2020-08-25 12:09:57 +1200

    Working on creating tables within the database

--- src/main/java/project/controller/Database.java
+++ src/main/java/project/controller/Database.java
@@ -1,15 +1,12 @@
 package project.controller;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
 
-    public static void createNewDatabase(String filename) {
+    public static void createNewDatabase() {
 
-        String url = "jdbc:sqlite:./data/" + filename;
+        String url = "jdbc:sqlite:./data/database.db";
 
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
@@ -22,9 +19,22 @@ public class Database {
         }
     }
 
+    public static void createAirportTable(String tableName) {
+        String url = "jdbc:sqlite:./data/database.db";
+        String sql = "CREATE TABLE IF NOT EXISTS "+tableName+" (\n"
+                ;
+
+        try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 
     public static void main(String[] args) {
-        createNewDatabase("test.db");
+        createNewDatabase();
     }
 
 }
