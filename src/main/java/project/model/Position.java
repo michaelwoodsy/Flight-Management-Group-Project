e111d153bb1c24a260576c84786a6e81b4a3ec73
Edward <ell30@uclive.ac.nz>
2020-09-23 22:19:05 +1200

    Added the code for JSON for maps api, commented out calling it, (will ask tutors why broken)

---
+++ src/main/java/project/model/Position.java
@@ -0,0 +1,12 @@
+package project.model;
+
+public class Position {
+//    from the Git lab, google maps
+    public double lat;
+    public double lng;
+
+    public Position(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
