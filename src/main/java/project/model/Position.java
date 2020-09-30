c1f382ca1699ccaef26ee98ee139c210bc72217c
Edward <ell30@uclive.ac.nz>
2020-09-30 14:38:01 +1300

    Added code for adding title for markers on map, hovering over shows name and title of airport

--- src/main/java/project/model/Position.java
+++ src/main/java/project/model/Position.java
@@ -4,9 +4,13 @@ public class Position {
 //    from the Git lab, google maps
     public double lat;
     public double lng;
+    public String name;
+    public String iata;
 
-    public Position(double lat, double lng) {
+    public Position(double lat, double lng, String name, String iata) {
         this.lat = lat;
         this.lng = lng;
+        this.name = name;
+        this.iata = iata;
     }
 }
